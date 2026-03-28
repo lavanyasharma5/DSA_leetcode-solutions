420a3c0b78f0c1901ad75abf84e1635ddb5d232e
Lavanya Sharma <your-real-email@gmail.com>
2026-03-29 00:57:32 +0530

    Solved Closest Number to Zero - Absolute comparison

---
+++ arrays/023_closest_number_to_zero.cpp
@@ -0,0 +1,37 @@
+/*
+Problem: Find Closest Number to Zero
+Idea:
+Compare absolute values.
+Smaller absolute value -> closer to 0
+If tie → choose positive number.
+
+Time: O(n)
+Space: O(1)
+*/
+
+#include<vector>
+#include<cmath>
+using namespace std;
+
+class soluion{
+    public:
+    int findClosestNumber(vector<int>& nums){
+        int ans= INT_MAX; //initialise ans to max value 
+
+        for(int i=0; i<nums.size(); i++){
+
+            // If the absolute value of the current element is less than the absolute value of ans
+            // we update ans to the current element.
+            if(abs(nums[i]) < abs(ans)){
+                ans= nums[i];
+            }
+            // If the absolute value of the current element is equal to the absolute value of ans 
+            // and the current element is greater than ans, we pick the greater number 
+            else if(abs(nums[i]) == abs(ans) && nums[i] > ans){
+                ans= nums[i];
+            }
+        }
+        return ans;
+    }
+
+};
